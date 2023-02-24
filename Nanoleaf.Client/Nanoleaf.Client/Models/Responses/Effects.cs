18c2bea4ce3e9981b0059f7c65952d01e12f33a8
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2023-02-24 15:31:39 +0100

    add comments

--- Nanoleaf.Client/Nanoleaf.Client/Models/Responses/Effects.cs
+++ Nanoleaf.Client/Nanoleaf.Client/Models/Responses/Effects.cs
@@ -3,11 +3,20 @@ using System.Text.Json.Serialization;
 
 namespace Nanoleaf.Client.Models.Responses
 {
+    /// <summary>
+    /// Effect informations
+    /// </summary>
     public class Effects
     {
+        /// <summary>
+        /// The name of the selected effect
+        /// </summary>
         [JsonPropertyName("select")]
         public string SelectedEffect { get; set; }
 
+        /// <summary>
+        /// List with names of effects
+        /// </summary>
         [JsonPropertyName("effectsList")]
         public List<string> EffectList { get; set; }
     }
