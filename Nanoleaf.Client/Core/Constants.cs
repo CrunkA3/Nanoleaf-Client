557e9f3f479c7fddd1a44e77b3ac8bbfee5c7f70
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2023-02-24 15:43:04 +0100

    file move

---
+++ Nanoleaf.Client/Core/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Nanoleaf.Client.Core
+{
+    internal struct Constants
+    {
+        public const int NanoleafMulticastPort = 1900;
+        public const int DefaultUnicastPort = 1901;
+        public const int NanoleafPort = 16021;
+    }
+}
--- Nanoleaf.Client/Nanoleaf.Client/Core/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nanoleaf.Client.Core
-{
-    internal struct Constants
-    {
-        public const int NanoleafMulticastPort = 1900;
-        public const int DefaultUnicastPort = 1901;
-        public const int NanoleafPort = 16021;
-    }
-}
