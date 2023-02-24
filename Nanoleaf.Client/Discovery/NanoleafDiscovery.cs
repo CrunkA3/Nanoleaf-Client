bfa68ee3d78ba4472472848c0b00b6f42ffd8468
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2023-02-24 15:54:07 +0100

    add comments

--- Nanoleaf.Client/Discovery/NanoleafDiscovery.cs
+++ Nanoleaf.Client/Discovery/NanoleafDiscovery.cs
@@ -5,13 +5,24 @@ using DeviceDiscovery.Models;
 
 namespace Nanoleaf.Client.Discovery
 {
+    /// <summary>
+    /// Discover Nanoleaf devices
+    /// </summary>
     public class NanoleafDiscovery : IDisposable
     {
         private bool _isDisposed;
         private readonly DiscoveryService _discoveryService = new DiscoveryService();
 
+        /// <summary>
+        /// Discovered nanoleaf devices
+        /// </summary>
         public List<NanoleafClient> NanoleafClients { get; } = new List<NanoleafClient>();
 
+        /// <summary>
+        /// Call this method to discover devices
+        /// </summary>
+        /// <param name="discoveryRequest"></param>
+        /// <returns></returns>
         public List<NanoleafClient> DiscoverNanoleafs(NanoleafDiscoveryRequest discoveryRequest)
         {
             var nanoleafDevices = _discoveryService.LocateDevices(discoveryRequest);
--- Nanoleaf.Client/Discovery/NanoleafDiscoveryRequest.cs
+++ Nanoleaf.Client/Discovery/NanoleafDiscoveryRequest.cs
@@ -4,8 +4,17 @@ using Nanoleaf.Client.Core;
 
 namespace Nanoleaf.Client.Discovery
 {
+    /// <summary>
+    /// Nanoleaf device discovery options
+    /// </summary>
     public class NanoleafDiscoveryRequest : MSearchRequest
     {
+
+        /// <summary>
+        /// Create a new instance of NanoleafDiscoveryRequest
+        /// </summary>
+        /// <param name="unicastPort"></param>
+        /// <param name="timeoutSeconds"></param>
         public NanoleafDiscoveryRequest(int unicastPort = Constants.DefaultUnicastPort, double timeoutSeconds = 5)
         {
             MulsticastPort = Constants.NanoleafMulticastPort;
